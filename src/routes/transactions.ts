b35f2fcba88f2247ca615a2eb9ce4de5ade53900
Giordano Borsato <you@example.com>
2025-03-24 20:05:40 -0300

    feat: Get Routes only returns transactions that the user made using cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,28 +2,49 @@ import { FastifyInstance } from "fastify";
 import { db } from "../database";
 import { z } from "zod";
 import crypto from "node:crypto";
+import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/", async () => {
-    const transactions = await db("transactions").select()
+  
 
-    return { transactions }
-  })
+  app.get("/", {
+    preHandler: [checkSessionIdExists]
+  },
+    async (request, response) => {
 
-  app.get("/:id", async (request) => {
+      const { sessionId } = request.cookies
+
+      const transactions = await db("transactions").select().where("session_id", sessionId)
+
+      return { transactions }
+    })
+
+  app.get("/:id", {
+    preHandler: [checkSessionIdExists]
+  }, async (request) => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
     const { id } = getTransactionParamsSchema.parse(request.params)
 
-    const transaction = await db("transactions").where("id", id).first()
+    const { sessionId } = request.cookies
+
+    const transaction = await db("transactions").where({ session_id: sessionId, id: id })
 
     return { transaction }
   })
 
-  app.get("/summary", async () => {
-    const summary = await db("transactions").sum("amount", { as: "Amount"}).first()
+  app.get("/summary", {
+    preHandler: [checkSessionIdExists]
+  }, async (request) => {
+
+    const { sessionId } = request.cookies
+
+    const summary = await db("transactions")
+      .sum("amount", { as: "Amount" })
+      .where("session_id", sessionId)
+      .first()
 
     return { summary }
   })
@@ -39,10 +60,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body,
     );
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = crypto.randomUUID()
+
+      response.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7 // 7 days,    
+      })
+    }
+
     await db("transactions").insert({
       id: crypto.randomUUID(),
       title,
       amount: type === "credit" ? amount : amount * -1,
+      session_id: sessionId
     });
 
     return response.status(201).send("Transação concluída com sucesso");
